ed2cb1d1e8f0ce3f61ee2c65bab22fcfdf475b42
Tom <tommyzhao111@gmail.com>
2017-08-12 23:29:51 -0400

    Pre-pre-alpha release
    Done so far: basic cube mechanics, health system, scoring, color
    variety, difficulty curve, stage changes, gui.

---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMenu : MonoBehaviour {
+
+	public Text play;
+	public bool isPlayingAnim = false;
+	public bool isDecreasing = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+//	public void Play(){
+//		isPlayingAnim = true;
+//		if (isPlayingAnim) {
+
+//		}
+//	}
+
+
+
+	public void startGame()
+	{
+		SceneManager.LoadScene ("testscene");
+	}
+
+	void IncreaseTextAnim(){
+		for(float i = play.transform.localScale.y; i < 1.5f; i += 0.02f){
+			play.transform.localScale = new Vector3 (play.transform.localScale.x, i, play.transform.localScale.z) * Time.deltaTime;
+		}
+	}
+
+	void DecreaseTextAnim(){
+		for (float y = play.transform.localScale.y; y > 1; y -= 0.02f) {
+			play.transform.localScale = new Vector3 (play.transform.localScale.x, y, play.transform.localScale.z) * Time.deltaTime;
+		}
+	}
+
+
+	public void Menu(){
+		Application.LoadLevel ("CubeSelect");
+	}
+}
---
+++ Assets/Scripts/uiManager.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class uiManager : MonoBehaviour
+{
+
+    public Canvas panel;
+
+    public Button[] buttons;
+    public Button newBest;
+    public Button right;
+    public Button left;
+    
+	private bool gameOver = false;
+    private int score = 0;
+    public int highScore;
+
+    //public Unlocks unlocker;
+    //	public GameObject greenQuad;
+    //	public GameObject greenCube;
+    //	public GameObject pinkCube;
+
+    public GameObject closeButton;
+    public GameObject whiteCube;
+    
+    public uiSelect uiSelect;
+
+	public Text endScore;
+	public Text endHighScore;
+	public Text unlockText;
+    public Text dead;
+
+    public Text rText;
+    public Text lText;
+
+	public Text scoreText;
+    public Text healthText;
+    public Text coinsText;
+
+    public GameObject cubeSelect;
+    //	public GameObject cubo;
+    //	public GameObject defaultCube;
+
+    // Use this for initialization
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+        gameOver = false;
+        
+        whiteCube.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+        endScore.text = "Score: " + score;
+        endHighScore.text = "High Score: " + highScore;
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+
+        if (score > highScore && gameOver == true)
+        {
+            highScore = score;
+            //		newBest.SetActive(true);
+        }
+        /*
+		if (uiSelect.greenSelected == true) {
+			whiteCube.gameObject.SetActive (false);
+			pinkCube.gameObject.SetActive (false);
+			greenCube.SetActive (true);
+			curvyCube.SetActive (false);
+		} else if (uiSelect.greenSelected == false && uiSelect.pinkSelected == false && uiSelect.curvySelected == false) {
+			whiteCube.gameObject.SetActive (true);
+		} else if (uiSelect.pinkSelected == true && uiSelect.greenSelected == false && uiSelect.curvySelected == false) {
+			whiteCube.gameObject.SetActive (false);
+			greenCube.SetActive (false);
+			pinkCube.SetActive (true);
+			curvyCube.SetActive (false);
+		} else if (uiSelect.pinkSelected == false && uiSelect.greenSelected == false && uiSelect.curvySelected == true) {
+			whiteCube.gameObject.SetActive (false);
+			pinkCube.gameObject.SetActive (false);
+			greenCube.gameObject.SetActive(false);
+			curvyCube.gameObject.SetActive(true);
+		}
+	*/
+        /*if (highScore > 200 && unlocker.greenUnlocked == false && gameOver == true) {
+			unlocker.greenUnlocked = true;
+			greenQuad.SetActive (true);
+			unlockText.gameObject.SetActive(true);
+			endScore.gameObject.SetActive (false);
+			endHighScore.gameObject.SetActive (false);
+			closeButton.SetActive (true);
+			greenCube.SetActive (true);
+			panel.enabled = false;
+			foreach (Button button in buttons) {
+				button.gameObject.SetActive (false);
+			}
+			PlayerPrefsX.SetBool ("greenUnlocked", true);
+		}*/
+    }
+
+    /*
+        public void CloseUnlockScreen()
+        {
+            greenQuad.SetActive (false);
+            greenCube.SetActive (false);
+            unlockText.enabled = false;
+            closeButton.SetActive (false);
+            panel.enabled = true;
+            endScore.gameObject.SetActive (true);
+            endHighScore.gameObject.SetActive (true);
+            foreach (Button button in buttons) {
+                button.gameObject.SetActive (true);
+            }
+        }
+    */
+    public void OpenCubeSelect()
+    {
+        panel.gameObject.SetActive(true);
+        cubeSelect.SetActive(true);
+    }
+
+	public void setScore(int sc){
+		score = sc;
+	}
+
+    public void CloseCubeSelect()
+    {
+        panel.gameObject.SetActive(false);
+        cubeSelect.SetActive(false);
+    }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.SetInt("highScore", highScore);
+    }
+
+    IEnumerator PlayLevel()
+    {
+        yield return new WaitForSeconds(0f);
+        SceneManager.LoadScene("testscene");
+    }
+
+    IEnumerator MenuLevel()
+    {
+        yield return new WaitForSeconds(0f);
+        SceneManager.LoadScene("menuScene");
+    }
+
+    public void gameOverOn()
+    {
+        StartCoroutine(gameOverActivated());
+    }
+
+    private IEnumerator gameOverActivated()
+    {
+        gameOver = true;
+        yield return new WaitForSeconds(0.01f);
+
+        dead.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+		
+		dead.gameObject.SetActive(false);
+		Destroy(dead.gameObject);
+
+        coinsText.gameObject.SetActive(false);
+        healthText.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(false);
+
+        endScore.gameObject.SetActive(true);
+        endHighScore.gameObject.SetActive(true);
+		if (score > highScore)
+			newBest.gameObject.SetActive (true);
+		else
+			newBest.gameObject.SetActive (false);	
+
+        panel.gameObject.SetActive(true);
+        right.gameObject.SetActive(false);
+        left.gameObject.SetActive(false);
+        rText.gameObject.SetActive(false);
+        lText.gameObject.SetActive(false);
+        //	greenCube.gameObject.SetActive (false);
+        foreach (Button button in buttons)
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+
+    public void Play()
+    {
+        StartCoroutine(PlayLevel());
+    }
+
+    public void Pause()
+    {
+
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+
+    public void Menu()
+    {
+        StartCoroutine(MenuLevel());
+    }
+
+}
